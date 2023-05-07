36543da3af20f4d8b05de07557ad7324843b6e9a
pablofrioli <88405559+pablofrioli@users.noreply.github.com>
2023-05-07 15:31:27 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+setup(name='fin_ml',
+version='0.2',
+description='Toolbox with util functions to apply Machine Learning to Finance',
+author='Pablo Frisanco Oliveira',
+packages=['fin_ml/transformers'],
+zip_safe=False)
